d8e75dbbca6f04c7ce59eaffb8c948bd47b6e554
VictorPerez-Dev <victorperez.dev.js@gmail.com>
2023-11-22 10:40:33 -0500

    Agregando proyecto

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h5>By Victor Perez</h5>
+        </div>
+    )
+}
+
+export default Footer
